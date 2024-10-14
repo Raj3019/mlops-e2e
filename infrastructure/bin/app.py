25a3809aabbcbb5d734a2178e9091a1ef63579ea
Raj3019 <rajchauhan3019@gmail.com>
2024-10-14 14:40:15 +0530

    add

--- infrastructure/bin/app.py
+++ infrastructure/bin/app.py
@@ -2,14 +2,15 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 from aws_cdk import App
-from lib.infrastractureStack import InfrastractureStack
+# from lib.infrastractureStack import InfrastractureStack
+from lib.infrastractureStack import InfrastructureStack
 import json
 
 with open('../../configuration/projectConfig.json') as config_file:
     project_config = json.load(config_file)
 
 app = App()
-InfrastractureStack(app, f'MLOpsInfrastractureStack-{project_config["projectName"]}', {
+InfrastructureStack(app, f'MLOpsInfrastractureStack-{project_config["projectName"]}', {
     'projectName': project_config['projectName'],
     'repoType': 'git' if project_config['repoType'] == 'git' else 'codecommit',
     'git': project_config['git'],
